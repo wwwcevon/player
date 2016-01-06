5bbdf0f6de2135d9ce5da6aca3eb8d3e983fa8d3
kevin <804625920@qq.com>
2016-01-06 10:02:38 +0800

    tests

---
+++ tests/play_a_song.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl 'http://127.0.0.1:5000/play' -X POST -H 'Accept: application/json, text/plain, */*' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.5' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Content-Length: 67' -H 'Content-Type: application/json;charset=utf-8' -H 'Host: 127.0.0.1:5000' -H 'Pragma: no-cache' -H 'Referer: http://127.0.0.1:5000/' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0' -d '{"song":"/home/kevin/Mine/music/经典/10.一剪梅-费玉清.mp3"}'
+
